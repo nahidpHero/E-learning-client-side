d3741976638bd3c05ce5e6d5796bd138d33cb40c
Nahidhassan1 <nahidlikhon22@gmail.com>
2022-10-25 11:05:29 +0600

    login form added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import './Login.css'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Login = () => {
+   const handleSignin=(event)=>{
+    event.preventDefault()
+    const form=event.target;
+    const email=form.email.value;
+    const password=form.name.value;
+    console.log(email,password)
+   }
+
     return (
-        <div>
-            <h1>this is login</h1>
-            
-        </div>
+        <div className='form-style'>
+     <form onSubmit={handleSignin}>
+      <Form.Group className="mb-3">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control name='email' type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Password</Form.Label>
+        <Form.Control name='password' type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </form>
+    </div>
     );
 };
 
